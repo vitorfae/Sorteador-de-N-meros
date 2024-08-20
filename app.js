fa876720532060f7e54a72803bbed1f9d6b949fc
Vitor Faé <vitorfaeulrich0@gmail.com>
2024-08-20 11:21:40 -0300

    Atualização de campos e funções referente a vaior maior que o limite

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ function sortear(){
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
+    if(de >= ate){
+        alert('Campo "Do número" deve ser inferior ao campo "Até o Número". Verifique!');
+        return valoresZerados();
+    }
+
     let sorteados = [];
     let numero; 
 
@@ -37,9 +42,13 @@ function alterarStatusDoBotao(){
 }
 
 function reiniciar() {
-    document.getElementById('quantidade').value = ''
-    document.getElementById('de').value = ''
-    document.getElementById('ate').value = ''
+    valoresZerados()
     document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados: Nenhum até agora </label>';
     alterarStatusDoBotao()
+}
+
+function valoresZerados(){
+    document.getElementById('quantidade').value = ''
+    document.getElementById('de').value = ''
+    document.getElementById('ate').value = ''  
 }
